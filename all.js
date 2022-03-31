666d3a843fc133f2454fbc22af742235fe18b84a
wendy03 <wendysu03@gmail.com>
2022-03-31 21:47:41 +0800

    fix image upload

--- all.js
+++ all.js
@@ -18,18 +18,29 @@ const quill = new Quill('#description', {
     },
     toolbar: {
       container: toolbarOptions,
-      handlers: {
-        image: imageHandler,
+      // handlers: {
+      //   image: imageHandler,
+      // },
+    },
+    imageUploader: {
+      upload: (file) => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(
+              'https://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+            );
+          }, 3000);
+        });
       },
     },
   },
   placeholder: '寫下產品描述',
 });
 
-function imageHandler() {
-  let range = this.quill.getSelection();
-  let value = prompt('please copy paste the image url here.');
-  if (value) {
-    this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
-  }
-}
+// function imageHandler() {
+//   let range = this.quill.getSelection();
+//   let value = prompt('please copy paste the image url here.');
+//   if (value) {
+//     this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
+//   }
+// }
